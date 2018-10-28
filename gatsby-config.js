5ad38ea4f7cbf06b032484aeedf10e4758df71fc
Raúl Ojeda <rauloojs@gmail.com>
2018-10-28 00:29:40 -0500

    Change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Raúl Ojeda',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,9 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi There!</h1>
+    <p>This is my brand new personal website</p>
+    <p>Now let's build something great.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
